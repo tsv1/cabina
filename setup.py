b5341c87171c942a4730489519f90d85df1397ad
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2020-06-18 22:21:25 +0700

    bump version → 0.3.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="cabina",
-    version="0.3.1",
+    version="0.3.2",
     description="Configuration with typed env vars",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",

d008d61582e86a8b0a0402354fc93a3022051027
Nikita Tsvetkov <tsv1@fastmail.com>
2023-08-05 18:33:23 +0400

    bump version → 1.0.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="cabina",
-    version="0.7.2",
+    version="1.0.0",
     description="Configuration with typed env vars",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
